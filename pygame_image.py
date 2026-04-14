1704023e5c4491b7f7af8e392db7281c9c7cbcce
c0a25078 <c0a2507863@edu.teu.ac.jp>
2026-04-14 15:47:35 +0900

    練習5と6：こうかとん移動

--- pygame_image.py
+++ pygame_image.py
@@ -13,15 +13,17 @@ def main():
     kk_img = pg.image.load("fig/3.png") #練習3
     kk_img = pg.transform.flip(kk_img, True, False) #練習3
     tmr = 0
+    x = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0]) #練習2
+        x = tmr
+        screen.blit(bg_img, [-x, 0]) #練習2
         screen.blit(kk_img, [300, 200]) #練習4
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
